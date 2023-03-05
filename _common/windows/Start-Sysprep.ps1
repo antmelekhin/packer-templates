20d604d6be77dd190dbf08536b3dd0dc13cb2238
Melekhin Anton <antmelekhin@gmail.com>
2023-03-05 23:44:40 +0400

    add sysprep script

---
+++ _common/windows/Start-Sysprep.ps1
@@ -0,0 +1,12 @@
+﻿& "$env:SystemRoot\System32\Sysprep\Sysprep.exe" /oobe /generalize /quiet /quit /unattend:"C:\Windows\System32\Sysprep\unattend.xml"
+
+while ($true) {
+    $ImageState = Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Setup\State | Select-Object ImageState
+    if ($ImageState.ImageState -ne 'IMAGE_STATE_GENERALIZE_RESEAL_TO_OOBE') {
+        Write-Output $ImageState.ImageState
+        Start-Sleep -Seconds 10 
+    }
+    else { break }
+}
+
+Stop-Computer -Force

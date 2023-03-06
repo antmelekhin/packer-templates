5d78d1e157d08825ae4bb65eee43b9c7287fa0e9
Melekhin Anton <antmelekhin@gmail.com>
2023-03-06 21:21:09 +0400

    fix cleanup script

--- _common/windows/Start-Cleanup.ps1
+++ _common/windows/Start-Cleanup.ps1
@@ -1,48 +1,29 @@
-$ErrorActionPreference = 'Stop'
+﻿$ErrorActionPreference = 'Stop'
 
 $SpaceAtStart = [Math]::Round( ((Get-WmiObject win32_logicaldisk | Where-Object { $_.DeviceID -eq $env:SystemDrive }).FreeSpace) / 1GB, 2)
-
-# Cleanup Windows Update area after all that
-Write-Output 'Cleaning up WinSxS updates'
-Start-Process -FilePath 'C:\Windows\system32\Dism.exe' -ArgumentList '/Online /Cleanup-Image /StartComponentCleanup /ResetBase' -NoNewWindow -Wait
-
-# Set registry keys for all the other cleanup areas we want to address with cleanmgr - fairly comprehensive cleanup
-if ((Get-ItemProperty -Path 'HKLM:\Software\Microsoft\Windows NT\CurrentVersion\').InstallationType -ne 'Server Core') {
-    $VolumeCaches = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches'
-    $CleanmgrGroups = @(
-        'Active Setup Temp Folders',
-        'Downloaded Program Files',
-        'Internet Cache Files',
-        'Memory Dump Files',
-        'Old ChkDsk Files',
-        'Previous Installations',
-        'Recycle Bin',
-        'Setup Log Files',
-        'System error memory dump files',
-        'System error minidump files',
-        'Temporary Files',
-        'Temporary Setup Files',
-        'Upgrade Discarded Files',
-        'Windows Error Reporting Archive Files',
-        'Windows Error Reporting Queue Files',
-        'Windows Error Reporting System Archive Files',
-        'Windows Error Reporting System Queue Files',
-        'Windows Upgrade Log Files'
-    )
-
-    $CleanmgrGroups | ForEach-Object { New-ItemProperty -Path "$VolumeCaches\$_" -Name 'StateFlags0012' -Value '2' -PropertyType 'DWord' }
-
-    # Run Cleanmgr utility
-    Write-Output 'Running CleanMgr with Sagerun:12'
-    Start-Process -FilePath 'C:\Windows\system32\cleanmgr.exe' -ArgumentList '/sagerun:12' -WindowStyle Hidden -Wait
-
-    Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\*' -Name 'StateFlags0012' | Remove-ItemProperty -Name "StateFlags0012" -ErrorAction:SilentlyContinue
-}
 
 # Clear downloaded updates
 Stop-Service -Name wuauserv -Force
 Remove-Item -Path "$env:windir\SoftwareDistribution\*" -ErrorAction:SilentlyContinue -Recurse
 Start-Service -Name wuauserv
+
+# Cleanup Windows Update area after all that
+Write-Output 'Cleaning up WinSxS updates'
+Start-Process -FilePath "$env:SystemRoot\system32\Dism.exe" -ArgumentList '/Online /Cleanup-Image /StartComponentCleanup /ResetBase' -NoNewWindow -Wait
+
+# Set registry keys for all the other cleanup areas we want to address with cleanmgr - fairly comprehensive cleanup
+if ((Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\').InstallationType -ne 'Server Core') {
+    $VolumeCaches = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches'
+    Get-ItemProperty -Path "$VolumeCaches\*" | ForEach-Object {
+        New-ItemProperty -Path $_.PSPath -Name 'StateFlags0012' -Value '2' -PropertyType 'DWord'
+    }
+
+    # Run Cleanmgr utility
+    Write-Output 'Running CleanMgr with Sagerun:12'
+    Start-Process -FilePath "$env:SystemRoot\system32\cleanmgr.exe" -ArgumentList '/sagerun:12' -NoNewWindow -Wait
+
+    Get-ItemProperty -Path "$VolumeCaches\*" -Name 'StateFlags0012' | Remove-ItemProperty -Name 'StateFlags0012'
+}
 
 # Clean up files
 Write-Output 'Clearing Files'
